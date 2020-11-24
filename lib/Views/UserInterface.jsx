c782641cb8a03fea7d8ab5daf24eb0a6faf77fea
Juan Emilio Zurita Macías <juan.macias@sa.catapult.org.uk>
2020-11-24 18:15:04 +0000

    [CSVS-155] Remove about button from User Interface (#14)

--- lib/Views/UserInterface.jsx
+++ lib/Views/UserInterface.jsx
@@ -32,9 +32,6 @@ function isBrowserSupportedAV() {
 export default function UserInterface(props) {
   return (
     <StandardUserInterface {...props} version={version}>
-      <Menu>
-        <MenuItem caption="About" href="about.html" key="about-link" />
-      </Menu>
       <Nav>
         <MeasureTool terria={props.viewState.terria} key="measure-tool" />
       </Nav>
